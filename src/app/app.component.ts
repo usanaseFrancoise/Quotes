14f1f2d903e8a5f8d590a8597334c21d4c9024b1
usanase Francoise <fusanasencoise@gmail.com>
2019-08-25 18:33:58 +0200

    edi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Quote} from './quote';
 })
 export class AppComponent {
   quotes:Quote[] = [
-    new Quote(1,'Inspiration Quotes','Fanny','The good you do today will be forgotten tomorrow.Do good anyway',new Date(2019,9,24),0,0),
-    new Quote(2,'Happiness Quotes','Fanny','Love is that condition in which the happiness of another person is essential to your own',new Date(2019,8,9),0,0),
+    new Quote(1,'Inspiration Quotes','Fanny','The good you do today will be forgotten tomorrow.Do good anyway',new Date(2018,1,24),0,0),
+    new Quote(2,'Happiness Quotes','Fanny','Love is that condition in which the happiness of another person is essential to your own',new Date(2018,7,9),0,0),
     new Quote(3,'Punctuation Quotes','Fanny','Arriving late was a way of saying that your own time was more valuable than the time of the person who waited for you',new Date(2019,1,9),0,0),
-    new Quote(4,'Loneliness Quotes','Fanny','Let me tell you this: if you meet a loner, no matter what they tell you, it is not because they enjoy solitude. It is because they have tried to blend into the world before, and people continue to disappoint them.',new Date(2019,12,4),0,0)
+    new Quote(4,'Loneliness Quotes','Fanny','Let me tell you this: if you meet a loner, no matter what they tell you, it is not because they enjoy solitude. It is because they have tried to blend into the world before, and people continue to disappoint them.',new Date(2019,5,4),0,0)
   ];
   private content: string;
     public query: string;
