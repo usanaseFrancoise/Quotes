844f2506c76f830e84762514c72bb2c0e8f6b1e9
usanase Francoise <fusanasencoise@gmail.com>
2019-08-25 19:09:11 +0200

    adding icons

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
  
     quotes:Quote[] = [
-      new Quote(1,'Inspiration Quotes','Fanny','The good you do today will be forgotten tomorrow.Do good anyway',new Date(2019,9,24),0,0),
-      new Quote(2,'Happiness Quotes','Fanny','Love is that condition in which the happiness of another person is essential to your own',new Date(2019,8,9),0,0),
-      new Quote(3,'Punctuation Quotes','Fanny','Arriving late was a way of saying that your own time was more valuable than the time of the person who waited for you',new Date(2019,1,9),0,0),
-      new Quote(4,'Loneliness Quotes','Fanny','Let me tell you this: if you meet a loner, no matter what they tell you, it is not because they enjoy solitude. It is because they have tried to blend into the world before, and people continue to disappoint them.',new Date(2019,12,4),0,0)
+      new Quote(1,'Inspiration Quotes','Fanny','The good you do today will be forgotten tomorrow.Do good anyway',new Date(2019,2,24),0,0),
+      new Quote(2,'Happiness Quotes','Fanny','Love is that condition in which the happiness of another person is essential to your own',new Date(2019,8,4),0,0),
+      new Quote(3,'Punctuation Quotes','Fanny','Arriving late was a way of saying that your own time was more valuable than the time of the person who waited for you',new Date(2019,1,6),0,0),
+      new Quote(4,'Loneliness Quotes','Fanny','Let me tell you this: if you meet a loner, no matter what they tell you, it is not because they enjoy solitude. It is because they have tried to blend into the world before, and people continue to disappoint them.',new Date(2019,4,4),0,0)
     ];
     addNewQuote(quote){
       let quoteLength = this.quotes.length;
